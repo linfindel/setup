d249cf7e6b3c58ff2c78d948942baf63ec626809
Nathan Hadley <116459177+linfindel@users.noreply.github.com>
2024-07-27 09:40:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo dnf config-manager --add-repo https://rpm.tuxedocomputers.com/fedora/40/x86_64/base
 
-sudo dnf install tuxedo-drivers
+sudo dnf install tuxedo-drivers --nogpgcheck
 
 sudo reboot now
